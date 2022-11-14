81ccc913967025b6414334950505fe4981525903
Javi <43324374+javierjrueda@users.noreply.github.com>
2022-11-14 13:13:27 +0100

    copied cog-themed-diffusion

---
+++ predict.py
@@ -0,0 +1,95 @@
+import os
+from typing import List
+
+import torch
+from diffusers import StableDiffusionPipeline
+from pytorch_lightning import seed_everything
+from cog import BasePredictor, Input, Path
+
+
+class Predictor(BasePredictor):
+    def setup(self):
+        """Load the model into memory to make running multiple predictions efficient"""
+        print("Loading pipeline...")
+
+        model_id = "prompthero/midjourney-v4-diffusion"
+        cache_dir = "midjourney-diffusion-cache"
+        self.pipe = StableDiffusionPipeline.from_pretrained(
+            model_id,
+            torch_dtype=torch.float32,
+            cache_dir=cache_dir,
+            local_files_only=True,
+        ).to("cuda")
+
+    @torch.inference_mode()
+    @torch.cuda.amp.autocast()
+    def predict(
+        self,
+        prompt: str = Input(
+            description="Input prompt",
+            default="mdjrny-v4 style a highly detailed matte painting of a man on a hill watching a rocket launch in the distance by studio ghibli, makoto shinkai, by artgerm, by wlop, by greg rutkowski, volumetric lighting, octane render, 4 k resolution, trending on artstation, masterpiece",
+        ),
+        width: int = Input(
+            description="Width of output image. Maximum size is 1024x768 or 768x1024 because of memory limits",
+            choices=[128, 256, 512, 768, 1024],
+            default=512,
+        ),
+        height: int = Input(
+            description="Height of output image. Maximum size is 1024x768 or 768x1024 because of memory limits",
+            choices=[128, 256, 512, 768, 1024],
+            default=512,
+        ),
+        num_outputs: int = Input(
+            description="Number of images to output", choices=[1, 4], default=1
+        ),
+        num_inference_steps: int = Input(
+            description="Number of denoising steps", ge=1, le=500, default=50
+        ),
+        guidance_scale: float = Input(
+            description="Scale for classifier-free guidance", ge=1, le=20, default=6
+        ),
+        seed: int = Input(
+            description="Random seed. Leave blank to randomize the seed", default=None
+        ),
+    ) -> List[Path]:
+        """Run a single prediction on the model"""
+
+        if seed is None:
+            seed = int.from_bytes(os.urandom(2), "big")
+        print(f"Using seed: {seed}")
+
+        if width == height == 1024:
+            raise ValueError(
+                "Maximum size is 1024x768 or 768x1024 pixels, because of memory limits. Please select a lower width or height."
+            )
+
+        seed_everything(seed)
+
+        output = self.pipe(
+            prompt=[prompt] * num_outputs,
+            width=width,
+            height=height,
+            num_inference_steps=num_inference_steps,
+            guidance_scale=guidance_scale,
+        )
+        samples = [
+            output["sample"][i]
+            for i, nsfw_flag in enumerate(output["nsfw_content_detected"])
+            if not nsfw_flag
+        ]
+
+        if len(samples) == 0:
+            raise Exception(
+                f"NSFW content detected. Try running it again, or try a different prompt."
+            )
+
+        print(
+            f"NSFW content detected in {num_outputs - len(samples)} outputs, showing the rest {len(samples)} images..."
+        )
+        output_paths = []
+        for i, sample in enumerate(samples):
+            output_path = f"/tmp/out-{i}.png"
+            sample.save(output_path)
+            output_paths.append(Path(output_path))
+
+        return output_paths
